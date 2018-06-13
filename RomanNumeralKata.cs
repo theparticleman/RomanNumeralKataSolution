ca2724d7081359502e854d297741d657b4b80786
Jon Turner <jonathan-turner@pluralsight.com>
2018-06-13 14:35:08 -0600

    Refactor 1 and 2 cases to make duplication more apparent

--- RomanNumeralKata.cs
+++ RomanNumeralKata.cs
@@ -20,8 +20,8 @@ namespace Tests
         internal static string Convert(int arabic)
         {
             if (arabic == 0) return "";
-            if (arabic == 1) return "I";
-            if (arabic == 2) return "II";
+            if (arabic == 1) return "I" + Convert(0);
+            if (arabic == 2) return "I" + Convert(1);
             return "V";
         }
     }
