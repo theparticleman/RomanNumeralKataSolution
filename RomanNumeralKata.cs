5d3f83e8848f5ac4dae495763a5c745a2f75801f
Jon Turner <jonathan-turner@pluralsight.com>
2018-06-13 14:41:55 -0600

    Delete unused code

--- RomanNumeralKata.cs
+++ RomanNumeralKata.cs
@@ -33,9 +33,6 @@ namespace Tests
             if (arabic == 0) return "";
             var conversion = lookup.First(x => x.arabic <= arabic);
             return conversion.roman + Convert(arabic - conversion.arabic);
-            // if (arabic >= 1 && arabic < 5) return "I" + Convert(arabic - 1);
-            // if (arabic >= 5 && arabic < 10) return "V" + Convert(arabic - 5);
-            // return "X";
         }
     }
 }
